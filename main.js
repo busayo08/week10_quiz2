0ed87b18c411b82b7a18a683ea1aa5457cc925f5
busayo08 <akinsolabusayo@gmail.com>
2018-11-09 18:37:58 +0100

    JV & HTML

---
+++ main.js
@@ -0,0 +1,14 @@
+var person = { 
+    name: ['firstName', 'lastName'], 
+    age: yourAge, 
+    gender: yourGender, 
+    interests: ['yourInterest', 'anotherInterest'], 
+    bio: function() { 
+        alert(this.name[0] + ' ' + this.name[1] + ' is ' + this.age + 
+        ' years old. She likes ' + this.interests[0] + ' and ' 
+        + this.interests[1] + '.');  
+    },  
+    greeting: function() {    
+        alert('Hi! I\'m ' + this.name[0] + '.');  
+    } 
+};
