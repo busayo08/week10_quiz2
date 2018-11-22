f5f4b428fa29361474c0b4a467a6969959f91b21
busayo08 <43090736+busayo08@users.noreply.github.com>
2018-11-22 17:38:03 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 var person = { 
-    name: ['firstName', 'lastName'], 
-    age: yourAge, 
-    gender: yourGender, 
-    interests: ['yourInterest', 'anotherInterest'], 
+    name: ['Akinsola', 'Busayo'], 
+    age: 23, 
+    gender: Female, 
+    interests: ['Football', 'Table tennis'], 
     bio: function() { 
         alert(this.name[0] + ' ' + this.name[1] + ' is ' + this.age + 
         ' years old. She likes ' + this.interests[0] + ' and ' 
